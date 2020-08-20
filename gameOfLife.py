5dcf558d518755a3d41042961b742210eef343c6
Kamil <kkowalczyk848@gmail.com>
2020-08-20 10:34:44 +0200

    updated

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,7 +1,7 @@
 import pygame
 import pygame.locals
 
-class Board(object):
+class Board(object): 
     def __init__(self, width, height): # constructor that will make board
         self.surface = pygame.display.set_mode((width, height), 0 ,32)
         pygame.display.set_caption("Game of Life")
@@ -14,7 +14,7 @@ class Board(object):
 
         pygame.display.update()
 
-class Game(object):
+class GameOfLife(object): # main class
     def __init__(self, width, height, cell_size = 10):
 
         pygame.init()
@@ -22,28 +22,34 @@ class Game(object):
         self.board = Board(width * cell_size, height * cell_size)
 
         self.fps_clock = pygame.time.Clock()
+        self.population = Population(width, height, cell_size)
 
     def run(self): # main loop of the game
         while not self.handle_events():
-            self.board.draw()
-            self.fps_clock.tick(15)
+            self.board.draw(self.population)
+            if getattr(self, "started", None):
+                self.population.cycle_generation()
+            self.fps_clock.tick(10)
 
     def handle_events(self):
-
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT:
                 pygame.quit()
                 return True
+            from pygame.locals import MOUSEMOTION, MOUSEBUTTONDOWN
+            if event.type == MOUSEMOTION or event.type == MOUSEBUTTONDOWN:
+                self.population.handle_mouse()
+            
+            from pygame.locals import KEYDOWN, K_RETURN
+            if event.type == KEYDOWN and event.key == K_RETURN:
+                self.started = True
         
-if __name__ == "__main__":
-    game = Game(80, 40)
-    game.run()
-    
+
+
 DEAD = 0
 ALIVE = 1
 
 class Population(object):
-
     def __init__(self, width, height, cell_size = 10):
         self.box_size = cell_size
         self.height = height
@@ -67,5 +73,53 @@ class Population(object):
         x /= self.box_size
         y /= self.box_size
 
-        #
+        # set state of cell in matrix
+        self.generation[int(x)][int(y)] = ALIVE if alive else DEAD
 
+    def draw_on(self, surface): # method used to draw cells
+            for x, y in self.alive_cells():
+                size = (self.box_size, self.box_size)
+                position = (x * self.box_size, y * self.box_size)
+                color = (46, 240, 161)
+                thickness = 2
+                pygame.draw.rect(surface, color, pygame.locals.Rect(position, size), thickness)
+        
+    def alive_cells(self): # returns co-ordinates of alive cells
+        for x in range(len(self.generation)):
+            column = self.generation[x]
+            for y in range(len(column)):
+                 if column[y] == ALIVE:
+                    yield x, y
+
+    def neighbours(self, x, y):
+        for nx in range(x-1, x+2):
+            for ny in range(y-1, y+2):
+                if nx == x and ny == y: # ignore center
+                    continue
+                if nx >= self.width:
+                    nx = 0
+                elif nx < 0:
+                    nx = self.width -1
+                if ny >= self.height:
+                    ny = 0
+                elif ny < 0:
+                    ny = self.height - 1
+                yield self.generation[nx][ny]
+    def cycle_generation(self): #generates another population of cells
+        next_gen = self.reset_generation()
+        for x in range(len(self.generation)):
+            column = self.generation[x]
+            for y in range(len(column)):
+                count = sum(self.neighbours(x, y))
+                if count == 3:
+                    next_gen[x][y] = ALIVE # spreading
+                elif count == 2: # moving on to another generation
+                    next_gen[x][y] = column[y]
+                else: # too many or too little neighbours to survive
+                    next_gen[x][y] = DEAD
+
+        self.generation = next_gen 
+
+if __name__ == "__main__":
+    game = GameOfLife(80, 40)
+    game.run()
